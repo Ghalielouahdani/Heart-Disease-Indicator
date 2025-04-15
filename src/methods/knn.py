8c7928d818a72befe4ef18dbd7a2de083e748ff9
Ghali Elouahdani <ghali.elouahdani@gmail.com>
2025-04-15 10:05:34 +0200

    method predict KNN

--- src/methods/knn.py
+++ src/methods/knn.py
@@ -9,10 +9,13 @@ class KNN(object):
         """
             Call set_arguments function of this class.
         """
+
         self.k = k
         self.task_kind =task_kind
 
     def fit(self, training_data, training_labels):
+
+
         """
             Trains the model, returns predicted labels for training data.
             Hint: Since KNN does not really have parameters to train, you can try saving the training_data
@@ -27,14 +30,16 @@ class KNN(object):
                 pred_labels (np.array): labels of shape (N,)
         """
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        self.trainingData = training_data
+        self.trainingLabels = training_labels
+        
+        
         return pred_labels
 
     def predict(self, test_data):
+        #on a test_data, données inconnues (N,D) --> on veut la classer
+        #on va comparer pour chaque n (0,N) sa distance avec les points de training_data (données connues)
+        #on prend k distances les plus petites avec le me
         """
             Runs prediction on the test data.
 
@@ -43,9 +48,17 @@ class KNN(object):
             Returns:
                 test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        test_labels = np.zeros(test_data.shape[0])
+
+        for i in range(test_data.shape[0]):
+            distances = np.sqrt(np.sum((self.trainingData - test_data[i])**2, axis=1))
+            nearest_neighbors = np.argsort(distances)[:self.k]
+            neighbor_labels = self.trainingLabels[nearest_neighbors]
+            if self.task_kind == "classification":
+                test_labels[i] = np.bincount(neighbor_labels.astype(int)).argmax()
+            else:
+                test_labels[i] = np.mean(neighbor_labels)
+
+
+
         return test_labels
