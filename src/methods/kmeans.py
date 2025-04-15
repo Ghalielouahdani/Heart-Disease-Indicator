600279e7b060c293e89d5ca1b05ffbc6f279fc11
nacer <nacersaadani2006@gmail.com>
2025-04-15 16:10:24 +0200

    Kmeans done

--- src/methods/kmeans.py
+++ src/methods/kmeans.py
@@ -7,15 +7,48 @@ class KMeans(object):
     kNN classifier object.
     """
 
-    def __init__(self, max_iters=500):
-        """
-        Call set_arguments function of this class.
-        """
+    def __init__(self, max_iters=500, n_clusters=5, n_init=10):
         self.max_iters = max_iters
+        self.n_clusters = n_clusters
+        self.n_init = n_init
         self.centroids = None
         self.best_permutation = None
 
     def fit(self, training_data, training_labels):
+        best_accuracy = 0
+        best_centroids = None
+        best_pred_labels = None
+
+        for _ in range(self.n_init):
+            
+            centroids = training_data[np.random.choice(training_data.shape[0], self.n_clusters, replace=False)]
+
+            for _ in range(self.max_iters):
+                
+                distances = np.linalg.norm(training_data[:, np.newaxis] - centroids, axis=2)
+                cluster_assignments = np.argmin(distances, axis=1)
+
+                
+                new_centroids = np.array([training_data[cluster_assignments == k].mean(axis=0) if np.any(cluster_assignments == k) else centroids[k] for k in range(self.n_clusters)])
+                if np.allclose(centroids, new_centroids):
+                    break
+                centroids = new_centroids
+
+            
+            pred_labels = np.zeros_like(cluster_assignments)
+            for k in range(self.n_clusters):
+                if np.any(cluster_assignments == k):
+                    pred_labels[cluster_assignments == k] = mode(training_labels[cluster_assignments == k])[0]
+
+            
+            accuracy = np.mean(pred_labels == training_labels)
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_centroids = centroids
+                best_pred_labels = pred_labels
+
+        self.centroids = best_centroids
+
         """
         Trains the model, returns predicted labels for training data.
         Hint:
@@ -29,13 +62,9 @@ class KMeans(object):
             pred_labels (np.array): labels of shape (N,)
         """
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        return best_pred_labels
+    
 
-        return pred_labels
 
     def predict(self, test_data):
         """
@@ -46,10 +75,8 @@ class KMeans(object):
         Returns:
             test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        distances = np.linalg.norm(test_data[:, np.newaxis] - self.centroids, axis=2)
+        cluster_assignments = np.argmin(distances, axis=1)
+        return cluster_assignments
 
-        return test_labels
+
